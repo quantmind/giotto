1d3d40cd1d0624f10270d1dbdb446cf5988571be
Luca Sbardella <luca.sbardella@gmail.com>
2014-12-18 18:08:05 +0000

    fixed x zooming

--- media/giotto/giotto.js
+++ media/giotto/giotto.js
@@ -2268,12 +2268,7 @@
             fillOpacity: 1,
             colorOpacity: 1,
             lineWidth: 2,
-            formatY: ',g3',
-            active: {
-                color: 'darker',
-                // Multiplier for lineWidth, set to 1 for no change
-                lineWidth: 1
-            }
+            formatY: ',g3'
         },
         point: {
             symbol: 'circle',
@@ -3153,6 +3148,8 @@
             d.group().setBackground(d, e);
             return d;
         };
+
+        d.inRange = noop;
 
         return d;
     }
@@ -4946,86 +4943,18 @@
         }
 
     });
-
-    function gridplugin (group, opts) {
-        var grid, gopts;
-
-        group.showGrid = function () {
-            if (!grid) {
-                // First time here, setup grid options for y and x coordinates
-                if (!gopts) {
-                    gopts = extend({
-                        position: 'top',
-                        size: 0,
-                        show: opts.xaxis.grid === undefined || opts.xaxis.grid
-                    }, opts.grid);
-                    opts.xaxis = gopts;
-                    opts.yaxis = extend({
-                        position: 'left',
-                        size: 0,
-                        show: opts.yaxis.grid === undefined || opts.yaxis.grid
-                    }, opts.grid);
-                }
-
-                opts.before = '*';
-                grid = group.paper().group(opts);
-                grid.element().classed('grid', true);
-                grid.xaxis().tickFormat(notick).scale(group.xaxis().scale());
-                grid.yaxis().tickFormat(notick).scale(group.yaxis().scale());
-                grid.add(rectangle).options(opts.grid);
-                if (opts.xaxis.show) grid.drawXaxis();
-                if (opts.yaxis.show) grid.drawYaxis();
-                grid.render();
-            } else
-                grid.clear().render();
-            return group;
-        };
-
-        group.hideGrid = function () {
-            if (grid)
-                grid.remove();
-            return group;
-        };
-
-        // The redering function of the grid rectangle
-        var rectangle = function () {
-            var width = grid.innerWidth(),
-                height = grid.innerHeight();
-
-            if (grid.type() === 'svg') {
-                // svg
-                var rect = grid.element().select('.grid-rectangle');
-
-                if (!rect.node())
-                    rect = grid.element()
-                                .append("rect")
-                                .attr("class", "grid-rectangle");
-
-                rect.attr("width", width).attr("height", height);
-                this.setBackground(rect);
-            } else {
-                // canvas
-                var ctx = grid.context();
-                ctx.beginPath();
-                ctx.rect(0, 0, width, height);
-                this.setBackground(ctx);
-                ctx.fill();
-            }
-
-            grid.xaxis().tickSize(-height, 0);
-            grid.yaxis().tickSize(-width, 0);
-        };
-
-        function notick () {return '';}
-    }
-
+    //
+    //  Add grid functionality
     g.paper.plugin('grid', {
         defaults: {
             color: '#333',
             colorOpacity: 0.3,
             fill: '#c6dbef',
             fillOpacity: 0.2,
-            lineWidth: 0.5
+            lineWidth: 0.5,
+            zoomx: false,
+            zoomy: false,
+            scaleExtent: [1, 10]
         },
         svg: gridplugin,
         canvas: gridplugin
@@ -5060,6 +4989,154 @@
                 chart.hideGrid();
         });
     });
+
+
+    function gridplugin (group, opts) {
+        var paper = group.paper(),
+            grid, gopts, zooming;
+
+        if (!paper.zoom)
+            paper.zoom = paperzoom(paper);
+
+        group.showGrid = function () {
+            if (!grid) {
+                // First time here, setup grid options for y and x coordinates
+                if (!gopts) {
+                    gopts = extend({
+                        position: 'top',
+                        size: 0,
+                        show: opts.xaxis.grid === undefined || opts.xaxis.grid
+                    }, opts.grid);
+                    opts.xaxis = gopts;
+                    opts.yaxis = extend({
+                        position: 'left',
+                        size: 0,
+                        show: opts.yaxis.grid === undefined || opts.yaxis.grid
+                    }, opts.grid);
+                }
+                opts.before = '*';
+                grid = group.paper().group(opts);
+                grid.element().classed('grid', true);
+                grid.xaxis().tickFormat(notick).scale(group.xaxis().scale());
+                grid.yaxis().tickFormat(notick).scale(group.yaxis().scale());
+                grid.add(rectangle).options(opts.grid);
+                if (opts.xaxis.show) grid.drawXaxis();
+                if (opts.yaxis.show) grid.drawYaxis();
+                grid.render();
+            } else
+                grid.clear().render();
+            return group;
+        };
+
+        group.hideGrid = function () {
+            if (grid)
+                grid.remove();
+            return group;
+        };
+
+
+        // The redering function for the grid
+        var rectangle = function () {
+            var width = grid.innerWidth(),
+                height = grid.innerHeight(),
+                type = grid.type(),
+                scalex, scaley, zoom;
+
+            if (type === 'svg') {
+                if (zooming) return;
+
+                var rect = grid.element().select('.grid-rectangle');
+
+                if (!rect.node())
+                    rect = grid.element()
+                                .append("rect")
+                                .attr("class", "grid-rectangle");
+
+                rect.attr("width", width).attr("height", height);
+                this.setBackground(rect);
+            } else {
+                // canvas
+                var ctx = grid.context();
+                ctx.beginPath();
+                ctx.rect(0, 0, width, height);
+                this.setBackground(ctx);
+                ctx.fill();
+            }
+
+            // Add zoom functionality - when not zooming!
+            if (!zooming) {
+
+                grid.xaxis().tickSize(-height, 0);
+                grid.yaxis().tickSize(-width, 0);
+
+                if (!zoom && (opts.grid.zoomx || opts.grid.zoomy)) {
+
+                    zoom = d3.behavior.zoom().on('zoom', function () {
+                        zooming = true;
+                        if (scalex) {
+                            var x1 = scalex.invert(opts.margin.left),
+                                x2 = scalex.invert(opts.size[0] - opts.margin.right);
+                            grid.xaxis().scale().domain([x1, x2]);
+                        }
+                        if (scaley) {
+                            var y1 = scalex.invert(opts.margin.top),
+                                y2 = scalex.invert(opts.size[1] - opts.margin.bottom);
+                            grid.yaxis().scale().domain([y1, y2]);
+                        }
+                        paper.render();
+                        zooming = false;
+                    });
+
+                    if (type === 'svg')
+                        zoom(grid.element());
+                    else
+                        zoom(grid.paper().canvas());
+
+                    var factor = grid.factor();
+
+                    if (factor === 1) {
+                        if (opts.grid.zoomx)
+                            zoom.x(grid.xaxis().scale());
+
+                        if (opts.grid.zoomy)
+                            zoom.y(grid.yaxis().scale());
+                    } else {
+
+                        if (opts.grid.zoomx) {
+                            scalex = grid.xaxis().scale().copy();
+                            var x1 = scalex.invert(-factor*opts.margin.left),
+                                x2 = scalex.invert(factor*(opts.size[0] - opts.margin.left));
+                            scalex.domain([x1, x2]).range([0, opts.size[0]]);
+                            zoom.x(scalex);
+                        }
+
+                        if (opts.grid.zoomy) {
+                            scaley = grid.yaxis().scale().copy();
+                            var y1 = scaley.invert(-factor*opts.margin.left),
+                                y2 = scaley.invert(factor*(opts.size[1] - opts.margin.bottom));
+                            scaley.domain([y1, y2]).range([opts.size[1], 0]);
+                            zoom.y(scaley);
+                        }
+                    }
+
+                    if (opts.grid.scaleExtent)
+                        zoom.scaleExtent(opts.grid.scaleExtent);
+                }
+            }
+        };
+    }
+
+    function notick () {return '';}
+
+    function paperzoom (paper) {
+        var zoom;
+
+        return function (_) {
+            if (!arguments.length) return zoom;
+            zoom = _;
+            return paper;
+        };
+    }
 
 
     g.contextMenu = function () {
@@ -5713,85 +5790,6 @@
         }
     });
     //
-    //  Add zoom functionality
-    g.paper.plugin('zoom', {
-        defaults: {
-            x: true,
-            y: true,
-            extent: [1, 10]
-        },
-
-        svg: function (paper, opts) {
-            var zoom;
-
-            paper.zoom = function (options) {
-                init();
-                if (options)
-                    extend(opts.zoom, options);
-                if (opts.zoom.x)
-                    zoom.x(paper.xAxis().scale());
-                if (opts.zoom.y)
-                    zoom.y(paper.yAxis().scale());
-                if (opts.zoom.extent)
-                    zoom.scaleExtent(opts.zoom.extent);
-                zoom.on('zoom', paper.render);
-                var g = paper.root().current();
-                g.call(zoom);
-                paper.showGrid();
-            };
-
-            // PRIVATE FUNCTIONS
-
-            function init () {
-                if (!zoom) {
-                    zoom = d3.behavior.zoom();
-                    opts.zoom = extend({}, opts.zoom, g.defaults.paper.zoom);
-                }
-            }
-        },
-
-        canvas: function (paper, opts) {
-            var zoom;
-
-            paper.zoom = function (options) {
-                init();
-                if (options)
-                    extend(opts.zoom, options);
-                if (opts.zoom.x)
-                    zoom.x(paper.xAxis().scale());
-                if (opts.zoom.y)
-                    zoom.y(paper.yAxis().scale());
-                if (opts.zoom.extent)
-                    zoom.scaleExtent(opts.zoom.extent);
-                //zoom.on('zoom', paper.render);
-                //var g = paper.root().current();
-                //g.call(zoom);
-                paper.showGrid();
-            };
-
-            // PRIVATE FUNCTIONS
-
-            function init () {
-                if (!zoom) {
-                    zoom = d3.behavior.zoom();
-                    opts.zoom = extend({}, opts.zoom, g.defaults.paper.zoom);
-                }
-            }
-        }
-    });
-
-    //
-    //  Add grid functionality to charts
-    g.viz.chart.plugin(function (chart, opts) {
-
-        chart.on('tick.zoom', function () {
-            //if (opts.zoom)
-            //    chart.paper().zoom(opts.zoom);
-        });
-    });
-
-
-    //
     //  Optional Angular Integration
     //  ==================================
     //
@@ -6170,10 +6168,10 @@
             return d;
         };
 
-        d.inRange = function (ex, ey) {
-            _draw(ctx);
-            return ctx.isPointInPath(ex, ey);
-        };
+        //d.inRange = function (ex, ey) {
+        //    _draw(ctx);
+        //    return ctx.isPointInPath(ex, ey);
+        //};
 
         return d;
 
@@ -6579,7 +6577,7 @@
                     y = opts.position === 'top' ? 0 : this.group().innerHeight();
                 else
                     x = opts.position === 'left' ? 0 : this.group().innerWidth();
-                ax.selectAll('*').remove();
+                //ax.selectAll('*').remove();
                 ax.attr("transform", "translate(" + x + "," + y + ")").call(axis);
                 ax.selectAll('line, path')
                      .attr('stroke', this.color)
